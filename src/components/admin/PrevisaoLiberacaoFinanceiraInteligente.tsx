e25079ef9b55aef2a85f96b8657512fe1a60ee6f
Guidoka7 <gquavx@gmail.com>
2026-08-26 14:37:33 -0300

    refactor: ativar nova agenda financeira com detalhes por cliente

--- src/components/admin/PrevisaoLiberacaoFinanceiraInteligente.tsx
+++ src/components/admin/PrevisaoLiberacaoFinanceiraInteligente.tsx
@@ -1,5 +1,3 @@
 "use client";
 
-// Componente oficial da agenda financeira. Mantido como alias único para
-// evitar que a rota administrativa carregue uma implementação legada.
-export { CicloLiberacaoFinanceiraV2 as PrevisaoLiberacaoFinanceiraInteligente } from "./CicloLiberacaoFinanceiraV2";
+export { PrevisaoLiberacaoFinanceiraInteligenteV2 as PrevisaoLiberacaoFinanceiraInteligente } from "./PrevisaoLiberacaoFinanceiraInteligenteV2";
